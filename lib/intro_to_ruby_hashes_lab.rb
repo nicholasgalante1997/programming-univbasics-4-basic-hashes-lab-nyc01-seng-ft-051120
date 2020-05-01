81022b725db84fceb0c81a63028eea0fb68bc38a
Nicholas Galante <ngalant2@binghamton.edu>
2020-05-01 11:49:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,8 +3,12 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+ my_hash = {
+   :starks => "Winterfell",
+   :lannisters => "Casterly Rock"
+ }
 end
+ 
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
